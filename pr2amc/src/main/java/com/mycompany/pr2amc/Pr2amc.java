61cce528223517e6e23e57a9077d002024abce7d
g10by <giovanniguedes63@gmail.com>
2024-12-04 13:20:08 +0100

    first commit

---
+++ pr2amc/src/main/java/com/mycompany/pr2amc/Pr2amc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pr2amc;
+
+/**
+ *
+ * @author usuario
+ */
+public class Pr2amc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
